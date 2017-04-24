cc928056776cf8629b80dc9580d2a2ee6d311f9f
Parinaz <parinaz.sa@gmail.com>
2017-04-23 20:40:17 -0700

    desgined a simple program to retrieve all submited responses and write those which have answers to a csv file which the path of the file is defined by the user

--- KeboolaProblem/KeboolaProblem/Requests.py
+++ KeboolaProblem/KeboolaProblem/Requests.py
@@ -1,4 +1,26 @@
-class Requests(object):
-    """description of class"""
-
-
+import requests
+import csv
+resp = requests.get('https://api.typeform.com/v1/form/PfSNFM?key=ac83034cfa742c0f79c26e9a612b4ba7e2aa0d3d')
+if resp.status_code != 200:
+    raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+else:
+    questions = resp.json()['questions'] #hold all questions
+    responses = resp.json()['responses'] #hold all responses
+    fields=[] #array to hold all quesion ids
+    for question in questions:
+        fields.append(question['id'])
+    filePath = input('Please enter the path of the CSV file that you want to store data in:')
+    
+    temp = {} #temp dic to store corresponding answer to question
+    with open(filePath, 'w', encoding='utf-8') as csvFile:
+        writer = csv.DictWriter(csvFile, fieldnames=fields)
+        for response in responses:
+            answers = response['answers']
+            if answers != {}:
+                for question in questions:
+                    questionId = question['id']
+                    if questionId in answers.keys():
+                        temp[questionId] = answers[questionId]
+                    else:
+                        temp[questionId] = 'NaN'
+                writer.writerow(temp)
